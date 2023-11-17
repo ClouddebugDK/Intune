601cfe8d6b1bc739b74b3a512801bf30d2bed6e3
ClouddebugDK <147859957+ClouddebugDK@users.noreply.github.com>
2023-11-17 15:48:28 +0100

    Updated script to match main

--- Start.ps1
+++ Start.ps1
@@ -36,4 +36,45 @@ do
     pause
 }
 
+until ($selection -eq 'q')function Show-Menu {
+    param (
+        [string]$Title = 'Intune Configuration'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Press '1' for: Update Collect Diagnostics Log."
+    Write-Host "2: Press '2' for this option."
+    Write-Host "3: Press '3' for this option."
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+do {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection) {
+        '1' {
+            'You chose option #1'
+            If (!(Test-path -path "C:\Temp" -ErrorAction SilentlyContinue)) {
+                New-Item -Path "C:\temp" -ItemType Directory -Force | Out-Null   
+            }
+            Set-Location -Path "C:\temp" 
+            wget https://aka.ms/intuneps1 -outfile IntuneODCStandAlone.ps1
+            powerShell -ExecutionPolicy Bypass -File .\IntuneODCStandAlone.ps1
+            Install-Module "Az.Storage" | Out-Null
+            Import-Module "Az.Storage" | Out-Null
+
+            $LogFile = Get-ChildItem -Path "C:\Temp" -Include "$env_computername_CollectedData*.zip" -Recurse
+
+        } 
+        '2' {
+            'You chose option #2'
+        } 
+        '3' {
+            'You chose option #3'
+        }
+    }
+    pause
+}
+
 until ($selection -eq 'q')
